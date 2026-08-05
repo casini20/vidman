cb88dfb82a237fffb6f9246c8a29f05ae8b93f19
casini20 <casvintges@gmail.com>
2026-08-05 08:05:32 +0000

    fix routes import

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 from database import init_db
-from routes import accounts, posts
+import accounts, posts
 import os
 
 @asynccontextmanager
